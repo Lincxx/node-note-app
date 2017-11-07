68b1d52c59afe3ad7f07ff1c45195b7fc58cd992
Jeremy Lincoln <bossco28@gmail.com>
2017-11-07 10:37:29 -0500

    Add in fs and os

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Starting app');
+
+//file system
+const fs = require('fs');
+const os = require('os');
+
+
+var user = os.userInfo();
+console.log(user)
+
+//option 2
+fs.appendFileSync('greetings.txt', 'Hello There');
+
+
+
