f346bc4d08e0b705317e838c246851f8781883c1
Jeremy Lincoln <bossco28@gmail.com>
2017-11-08 09:17:46 -0500

    add in package lodash

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ console.log('Starting app.js');
 const fs = require('fs');
 //operating system
 const os = require('os');
+//lodash
+const _ = require('lodash');
 //requiring my own files
 const notes = require('./notes');
 
-var res = notes.add(3, 4);
-console.log(res);
+// console.log(_.isString(true))
+// console.log(_.isString("Jeremy"));
+var filteredArray = _.uniq(['1', 2, "Jeremy", 2, 4, "Jeremy"]);
+console.log(filteredArray);
+
+// var res = notes.add(3, 4);
+// console.log(res);
 
 // var user = os.userInfo();
 
