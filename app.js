430b8a7fd029f50371e1e05635eaaed795a7636b
Jeremy Lincoln <bossco28@gmail.com>
2017-11-09 08:44:56 -0500

    Modified app and note with command function

--- app.js
+++ app.js
@@ -3,10 +3,32 @@ console.log('Starting app.js');
 //Node Module
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 
 //Own files
 const notes = require('./notes');
 
+//access cmd line args
+const argv = yargs.argv;
+var command = argv._[0];
+console.log('Command: ' + `${command}`);
 
+console.log('YARGS ',  argv);
+
+if(command === 'add'){
+    //add a new note
+    notes.addNote(argv.title, argv.body);
+} else if(command === 'list'){
+    //show notes
+   notes.getAll();
+} else if(command === 'read'){
+    //read note
+    notes.getNote(argv.title);
+} else if(command === 'remove'){
+    //remove note
+    notes.removeNote(argv.title);
+} else {
+    console.log('Command not recognized');
+}
 
--- notes.js
+++ notes.js
@@ -1,9 +1,32 @@
 console.log('Starting notes.js');
 
 
-module.exports.addNote = () => {
-    console.log('add note');
-    return 'New note';
+var addNote = (title, body) =>{
+    console.log('adding note: ', title, body)
 };
 
+var getAll = () => {
+    console.log('Getting all notes');
+}
 
+var getNote = (title) => {
+    console.log('Fetching note: ', title);
+}
+
+var removeNote = (title) => {
+    console.log('Removing note: ', title);
+}
+
+//es5
+// module.exports = {
+//     addNote: addNote
+// } the below is the same
+
+//es6
+module.exports = {
+    //Only works if the obj attr and value are the same
+    addNote,
+    getAll,
+    getNote,
+    removeNote
+}
