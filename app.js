a523d44905eb9d157ff23cb381e55c16d5c8623f
Jeremy Lincoln <bossco28@gmail.com>
2017-11-13 08:50:39 -0500

    Config yargs to have require and helpers

--- app.js
+++ app.js
@@ -7,8 +7,31 @@ const yargs = require('yargs');
 //Own files
 const notes = require('./notes');
 
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
 //access cmd line args
-const argv = yargs.argv;
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'Remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 
 
